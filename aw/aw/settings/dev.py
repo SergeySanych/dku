904f40dd1be63e869630eba6ed22892eaecbdd97
SergeySanych <sk@i-fabrika.kz>
2022-12-14 17:28:47 +0600

    14-12-2022 Подключен постгресс на удаленном сервере

---
+++ aw/aw/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-ugv6@a^=o)@fjjkla)aw_%wolhj&dhaz8cb^9=mom!3yvk3k%*"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
