853d2b45a306eccd9b1c1678156811dd911d72ae
SergeySanych <sk@i-fabrika.kz>
2024-09-15 14:42:14 +0500

    15-09-2024 SLUN 2 раздела бетта

--- aw/minisite/models.py
+++ aw/minisite/models.py
@@ -10,7 +10,8 @@ from wagtail.images.blocks import ImageChooserBlock
 
 
 class LeftHeaderBlock(blocks.StructBlock):
-    left = blocks.CharBlock(required=False)
+    left = blocks.CharBlock(help_text='Заголовок блока', required=False)
+    leftrich = blocks.RichTextBlock(help_text='Форматированый блок под заголовком слева', required=False)
     header_level = blocks.IntegerBlock(help_text='Уровень заголовка', min_value=1, max_value=4, default=3)
     # 1 - blue, 2 - red
     color = blocks.IntegerBlock(help_text='0 - no, 1 - blue, 2 - red', default=0)
@@ -89,6 +90,46 @@ class CheckListBlock(blocks.StructBlock):
         label = 'CheckList'
 
 
+class FilterBlock(blocks.StructBlock):
+    filterlevel = blocks.IntegerBlock(help_text='1 or 2 - levels', default=1)
+
+    filterlist1 = blocks.ListBlock(
+        blocks.StructBlock(
+            [
+                ("fl1_name", blocks.TextBlock(help_text='Название фильтра')),
+                ("fl1_code", blocks.TextBlock(help_text='Код фильтра уникальный латинскими буквами, короткий')),
+            ]
+        )
+    )
+
+    filterlist2 = blocks.ListBlock(
+        blocks.StructBlock(
+            [
+                ("fl2_name", blocks.TextBlock(help_text='Название фильтра')),
+                ("fl2_code", blocks.TextBlock(help_text='Код фильтра уникальный латинскими буквами, короткий')),
+                ("fl2_fl1_code", blocks.TextBlock(help_text='Код фильтра уникальный латинскими буквами, короткий')),
+            ]
+        )
+    )
+
+    filterlistdata = blocks.ListBlock(
+        blocks.StructBlock(
+            [
+                ("fld_name", blocks.TextBlock(help_text='Название')),
+                ("fld_code1", blocks.TextBlock(help_text='Код из первого списка')),
+                ("fld_code2", blocks.TextBlock(help_text='Код из первого списка', required=False)),
+                ("fld_url", blocks.TextBlock(help_text='Ссылка на сайт', required=False)),
+                ("fld_logo", ImageChooserBlock(help_text='Логотип квадратный', required=False)),
+            ]
+        )
+    )
+
+    class Meta:
+        template = 'filter.html'
+        icon = 'check'
+        label = 'Filter'
+
+
 @register_snippet
 class MinisiteList(ClusterableModel):
     minisite_name = models.CharField(max_length=50)
@@ -97,6 +138,22 @@ class MinisiteList(ClusterableModel):
     minisite_subheader_ru = models.CharField(max_length=250, null=True, blank=True)
     minisite_subheader_en = models.CharField(max_length=250, null=True, blank=True)
 
+    minisite_listlogo = models.BooleanField(verbose_name="Показывать список логотипов", default=True)
+
+    minisite_fonblock = models.BooleanField(verbose_name="Показывать блок с фоновым рисунком и заголовком сайта", default=True)
+    minisite_fon_color = models.CharField(max_length=15, null=True, blank=True, default='#fafafa')
+    minisite_fon_textcolor = models.CharField(max_length=15, null=True, blank=True, default='#2165AD')
+
+    minisite_leftlogo = models.BooleanField(verbose_name="Показывать блок с логотипом слева и заголовком сайта", default=False)
+    minisite_ll_fon_color = models.CharField(max_length=15, null=True, blank=True, default='#fafafa')
+    minisite_ll_textcolor = models.CharField(max_length=15, null=True, blank=True, default='#2165AD')
+
+    minisite_mainlogo = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
+
+
     panels = [
         FieldPanel('minisite_name', heading="Короткое название сайта, как в домене"),
         FieldPanel('minisite_header_ru', heading="Заголовок мини сайта на русском"),
@@ -104,7 +161,35 @@ class MinisiteList(ClusterableModel):
         FieldPanel('minisite_subheader_ru', heading="ПодЗаголовок мини сайта на русском"),
         FieldPanel('minisite_subheader_en', heading="ПодЗаголовок мини сайта на английском"),
 
-        InlinePanel('minisite_gallery_images', label="Список логотипов"),
+
+        MultiFieldPanel(
+            [
+                FieldPanel('minisite_listlogo', heading="Показывать список логотипов"),
+                InlinePanel('minisite_gallery_images', label="Список логотипов"),
+            ],
+            heading="Блок логотипов",
+            classname="collapsed",
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel('minisite_fonblock', heading="Показывать блок с фоновым рисунком и заголовком сайта"),
+                FieldPanel('minisite_fon_color', heading="Цвет фона в шестнадцетеричном формате"),
+                FieldPanel('minisite_fon_textcolor', heading="Цвет текста в шестнадцетеричном формате"),
+            ],
+            heading="Блок с фоновым рисунком и заголовком сайта",
+            classname="collapsed",
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel('minisite_leftlogo', heading="Показывать блок с логотипом слева и заголовком сайта"),
+                FieldPanel('minisite_mainlogo', heading="Основной логотип минисайта"),
+                FieldPanel('minisite_ll_fon_color', heading="Цвет фона в шестнадцетеричном формате"),
+                FieldPanel('minisite_ll_textcolor', heading="Цвет текста в шестнадцетеричном формате"),
+            ],
+            heading="Блок с логотипом слева и заголовком сайта",
+            classname="collapsed",
+        ),
+
     ]
 
     def __str__(self):
@@ -125,7 +210,7 @@ class MinisitePage(Page):
 
     minisite = models.ForeignKey(
         MinisiteList,
-        on_delete=models.CASCADE,
+        on_delete=models.PROTECT,
         related_name='minisite_pages'
     )
 
@@ -136,24 +221,62 @@ class MinisitePage(Page):
 
     minisite_title_h1 = models.CharField(max_length=255, null=True, blank=True)
     minisite_news = models.BooleanField(verbose_name="Показывать новости", default=False)
+    minisite_onecolumn = models.BooleanField(verbose_name="Показывать блок во всю ширину экрана", default=False)
+    minisite_twocolumn = models.BooleanField(verbose_name="Показывать блок в две колонки", default=False)
 
     minisite_body = StreamField([
             ('heading', blocks.CharBlock(form_classname="subtitle")),
             ('paragraph', blocks.RichTextBlock()),
             ('image', ImageChooserBlock()),
             ('htmlcode', blocks.RawHTMLBlock()),
-            ('image', ImageChooserBlock()),
+            ('filter', FilterBlock()),
             ('leftheader2', LeftHeaderBlock()),
             ('checklist2', CheckListBlock2()),
         ], use_json_field=True, blank=True)
 
+    minisite_leftbody = StreamField([
+            ('heading', blocks.CharBlock(form_classname="subtitle")),
+            ('paragraph', blocks.RichTextBlock()),
+            ('image', ImageChooserBlock()),
+            ('htmlcode', blocks.RawHTMLBlock()),
+            ('filter', FilterBlock()),
+            ('leftheader2', LeftHeaderBlock()),
+            ('checklist2', CheckListBlock2()),
+        ], use_json_field=True, blank=True)
+
+    minisite_rightbody = StreamField([
+        ('heading', blocks.CharBlock(form_classname="subtitle")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('htmlcode', blocks.RawHTMLBlock()),
+        ('filter', FilterBlock()),
+        ('leftheader2', LeftHeaderBlock()),
+        ('checklist2', CheckListBlock2()),
+    ], use_json_field=True, blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('minisite',  heading='Выбор мини-сайта'),
         FieldPanel('minisite_bgimage', heading='Фоновый рисунок'),
         FieldPanel('minisite_title_h1', heading='Заголовок страницы'),
         FieldPanel('minisite_news', heading='Показывать новости'),
-        FieldPanel('minisite_body', heading='Контент блок'),
+        MultiFieldPanel(
+            [
+                FieldPanel('minisite_onecolumn', heading="Показывать блок во всю ширину экрана"),
+                FieldPanel('minisite_body', heading='Контент блок'),
+            ],
+            heading="Блок во всю ширину экрана",
+            classname="collapsed",
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel('minisite_twocolumn', heading="Показывать блок в две колонки"),
+                FieldPanel('minisite_leftbody', heading='Контент блок для левой колонки'),
+                FieldPanel('minisite_rightbody', heading='Контент блок для правой колонки'),
+            ],
+            heading="Блок в две колонки",
+            classname="collapsed",
+        ),
+
     ]
 
 
